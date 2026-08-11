8bd263c5e365c4d3c10b89e2426fc7bf7fc45675
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-08-11 19:12:53 -0400

    convert some ::value to _v

--- libcudacxx/include/cuda/std/__type_traits/add_pointer.h
+++ libcudacxx/include/cuda/std/__type_traits/add_pointer.h
@@ -36,7 +36,7 @@ template <class _Tp>
 using add_pointer_t _CCCL_NODEBUG_ALIAS = _CCCL_BUILTIN_ADD_POINTER(_Tp);
 
 #else // ^^^ _CCCL_BUILTIN_ADD_POINTER ^^^ / vvv !_CCCL_BUILTIN_ADD_POINTER vvv
-template <class _Tp, bool = __cccl_is_referenceable<_Tp>::value || is_void<_Tp>::value>
+template <class _Tp, bool = __cccl_is_referenceable<_Tp>::value || is_void_v<_Tp>>
 struct __add_pointer_impl
 {
   using type _CCCL_NODEBUG_ALIAS = remove_reference_t<_Tp>*;
